6fe07adcf7277c9ad55fcb5e7d1d1647401a101b
tiefseezeitung <beyza_tercan@hotmail.com>
2021-03-12 12:52:05 +0100

    clean up

--- Iso_Entities_Classification/hyperparameter_tuning.py
+++ Iso_Entities_Classification/hyperparameter_tuning.py
@@ -30,7 +30,7 @@ tag_type = "iso"
 search_space = SearchSpace()
 
 search_space.add(Parameter.EMBEDDINGS, hp.choice, options=[ 
-    WordEmbeddings("glove")
+    StackedEmbeddings([WordEmbeddings("glove"),FlairEmbeddings('news-forward'), FlairEmbeddings('news-backward')])
 ])
 # another embedding that could be tried
 #WordEmbeddings("glove")
@@ -39,8 +39,8 @@ search_space.add(Parameter.EMBEDDINGS, hp.choice, options=[
 #TransformerWordEmbeddings('distilbert-base-cased', fine_tune=True)
 
 search_space.add(Parameter.HIDDEN_SIZE, hp.choice, options=[64, 128])
-search_space.add(Parameter.RNN_LAYERS, hp.choice, options=[1, 2])
-search_space.add(Parameter.LEARNING_RATE, hp.choice, options=[0.1, 0.2, 0.3])
+search_space.add(Parameter.RNN_LAYERS, hp.choice, options=[1,2])
+search_space.add(Parameter.LEARNING_RATE, hp.choice, options=[0.1, 0.15, 0.2])
 search_space.add(Parameter.MINI_BATCH_SIZE, hp.choice, options=[8])
 
 #search_space.add(Parameter.DROPOUT, hp.uniform, low=0.0, high=0.5)
@@ -51,13 +51,13 @@ from flair.hyperparameter.param_selection import SequenceTaggerParamSelector, Op
 param_selector = SequenceTaggerParamSelector(
     corpus, 
     'iso', 
-    'resources_param_selector/results_glove_2', 
+    'resources_param_selector/results_flair', 
     max_epochs=15,
     training_runs=2,
     optimization_value=OptimizationValue.DEV_SCORE
 )
 
 # start the optimization
-param_selector.optimize(search_space, max_evals=30)
+param_selector.optimize(search_space, max_evals=20)
 
 
